f8e33973a6d72474f6e8e43326eb0966cee25c9b
lily <2238031420@qq.com>
2022-09-01 13:20:59 +0800

    提交测试

---
+++ threeproject/src/views/OrderManagment.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="order-mgt-title">
+            <h2>订单管理</h2>
+            <h2>提交测试</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'OrderManagment',
+}
+</script>
+
+<style scoped>
+    /* 订单管理title */
+    .order-mgt-title{
+        width: 100%;
+        height: 100px;
+        border-bottom: 1px solid gray;
+        line-height: 100px;
+    }
+</style>
