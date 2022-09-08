872147e948ba25b53a6817d760f7787076d03449
大白 <2591713645@qq.com>
2022-09-09 00:11:46 +0800

    补充

--- threeproject/vue.config.js
+++ threeproject/vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  lintOnSave: false,
+  transpileDependencies: true,
+  // 配置跨域
+  devServer: {
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://42.192.152.16:8080/doc.html",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "",
+        },
+      },
+    }
+  }
 })
